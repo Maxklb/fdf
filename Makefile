fc956622ea31cf935a2dcb19a0f724710327010a
Maxklb <themaxklb@gmail.com>
2024-04-25 13:29:43 +0200

    test

--- Makefile
+++ Makefile
@@ -6,7 +6,39 @@
 #    By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/04/23 15:22:16 by makoch-l          #+#    #+#              #
-#    Updated: 2024/04/23 15:22:17 by makoch-l         ###   ########.fr        #
+#    Updated: 2024/04/25 13:28:10 by makoch-l         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+NAME = fdf
+
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror
+RM = rm -f
+AR = ar rcs
+
+SRC_PATH = src/
+LIBFT_PATH = libft/
+LIBFT_LIB = libft.a
+INCLUDE_PATH = include/
+MLX_PATH = minilibx-linux/
+
+SRC_FILES = src/main.c \
+			ft_fdf.c   \
+
+SRC_OBJS= $(SRC_FILES:.c=.o)
+
+LIBFT_OBJS = $(LIBFT_PATH)*.OBJS
+LIBFTMAKE = $(MAKE) -C $(LIBFT_PATH)
+
+all : $(NAME)
+
+clean :
+	$(RM) $(SRC_OBJS)
+
+fclean : clean
+			$(RM) $(NAME)
+
+re : fclean all
+
+.PHONY : all clean fclean re pmake
--- src/main.c
+++ src/main.c
@@ -1,25 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fdf.h                                              :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/25 13:16:02 by makoch-l          #+#    #+#             */
-/*   Updated: 2024/04/25 13:28:40 by makoch-l         ###   ########.fr       */
+/*   Created: 2024/04/25 13:15:34 by makoch-l          #+#    #+#             */
+/*   Updated: 2024/04/25 13:15:58 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FDF_H
-# define FDF_H
+#include "../include/fdf.h"
 
-#include "../minilibx-linux/mlx.h"
-#include "../libft/libft.h"
-
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <math.h>
-#include <fcntl.h>
-
-#endif
