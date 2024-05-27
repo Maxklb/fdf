81756c350bd2c466ac2f91f6dc579eee7dc50f34
Maximilien Koch Le Breton <themaxklb@gmail.com>
2024-05-27 15:22:25 +0200

    prout

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: makoch-l <makoch-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 13:15:34 by makoch-l          #+#    #+#             */
-/*   Updated: 2024/05/27 14:34:09 by makoch-l         ###   ########.fr       */
+/*   Updated: 2024/05/27 14:45:48 by makoch-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,16 @@ int	main(void)
 		return (0);
 
 	map->mlx_ptr = mlx_init();
-	map->win_ptr = mlx_new_window(map->mlx_ptr, 100, 100, "fdf");
+	map->win_ptr = mlx_new_window(map->mlx_ptr, 300, 300, "fdf");
 	
-	map->img = mlx_new_image(map->mlx_ptr, 100, 100);
+	map->img = mlx_new_image(map->mlx_ptr, 300, 300);
+	
 	map->addr = mlx_get_data_addr(map->img, &map->bits_per_pixel, &map->line_length, &map->endian);
-	my_mlx_pixel_put(map, 50, 50, WHITE);
-	mlx_put_image_to_window(map->mlx_ptr, map->win_ptr, map->img, 0, 0);
+
+	my_mlx_pixel_put(map, 150, 150, 0x00FF0000);
+	my_mlx_pixel_put(map, 151, 150, 0x00FF0000);
+	my_mlx_pixel_put(map, 152, 150, 0x00FF0000);
+	mlx_put_image_to_window(map->mlx_ptr, map->win_ptr, &map->img, 0, 0);
+	
 	mlx_loop(map->mlx_ptr);
 }
